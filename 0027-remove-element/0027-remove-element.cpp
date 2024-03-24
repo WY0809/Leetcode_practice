338e58cf521716c3c6be3f728c827a5bd1376791
WY0809 <105807155+WY0809@users.noreply.github.com>
2024-03-24 13:21:57 +0800

    Time: 0 ms (100%), Space: 10.4 MB (80.37%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -3,11 +3,8 @@ public:
     int removeElement(vector<int>& nums, int val) {
         vector<int>::iterator it = nums.begin();
         int k = 0;
-        // nums.erase(it+1);
-        // nums.erase(it+2);
         for(int i = nums.size()-1; i > -1; --i){
             if(nums[i] == val){
-                cout<<i<<endl;
                 nums.erase(it+i);
             }else{
                 k++;
