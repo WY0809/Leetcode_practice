15658e32bcf1571ebaea06cbbc5aa435e5d337d0
WY0809 <105807155+WY0809@users.noreply.github.com>
2024-03-26 10:06:10 +0800

    Time: 16 ms (89.59%), Space: 28.3 MB (85.27%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
         int left = 0 , right = nums.size()-1;
         vector<int> ans(nums.size());
         for(int i = nums.size()-1; i>=0 ;--i){
-            if(nums[left]*nums[left] > nums[right]*nums[right]){
+            if(abs(nums[left]) > abs(nums[right])){
                 ans[i] = nums[left]*nums[left];
                 left++;
             }else{
