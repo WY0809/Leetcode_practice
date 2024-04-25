39ddaafe8be8ef19efcf6af4ba5a53a653090882
WY0809 <105807155+WY0809@users.noreply.github.com>
2024-04-26 02:02:19 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,17 +1,23 @@
-
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k % n; // Ensure k is within the range [0, n)
-
-        // Reverse the entire array
-        reverse(nums.begin(), nums.end());
-        
-        // Reverse the first k elements
-        reverse(nums.begin(), nums.begin() + k);
-        
-        // Reverse the rest of the elements after k
-        reverse(nums.begin() + k, nums.end());
+        int length = nums.size();
+        vector<int> temp(length);
+        while(k > 0)
+        {
+            for(int i = 0; i < length ; i++)
+            {
+                if(i==0)
+                {
+                    temp[i] = nums[length - 1];
+                }
+                else
+                {
+                    temp[i] = nums[i-1];
+                }
+            }
+            k--; 
+            nums = temp;
+        }
     }
 };
