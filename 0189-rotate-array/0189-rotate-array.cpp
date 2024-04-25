676698759424a551d99f8ad0bc6ee0de9d5880f6
WY0809 <105807155+WY0809@users.noreply.github.com>
2024-04-26 02:04:23 +0800

    Time: 19 ms (71.63%), Space: 28 MB (26.05%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,6 +3,7 @@ public:
     void rotate(vector<int>& nums, int k) {
         int length = nums.size();
         vector<int> temp(length);
+        k %= length;
         for(int i = 0; i < length ; i++)
         {
             temp[(i+k)%length] = nums[i];
