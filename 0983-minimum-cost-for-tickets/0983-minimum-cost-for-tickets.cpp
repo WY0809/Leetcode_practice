c987a3bc9471c0de9b356e3869ea79db4e733871
WY0809 <105807155+WY0809@users.noreply.github.com>
2024-04-27 00:55:13 +0800

    Time: 0 ms (100%), Space: 11.6 MB (81.5%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int length = days.size();
+        vector<int> dp(days[length-1]+1);
+        int j = 0;
+        for(int i = 1 ; i <= days[length-1] ; i++){
+            if( i == days[j]){
+                dp[i] = min({dp[max(0,i-1)] + costs[0], dp[max(0,i-7)] + costs[1], dp[max(0,i-30)] + costs[2]});
+                j++;
+            }else{
+                dp[i] = dp[i-1];
+            }
+        }
+        return dp[days[length-1]];
+    }
+};
