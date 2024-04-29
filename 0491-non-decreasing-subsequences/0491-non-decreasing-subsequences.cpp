0717c791835ad613045de098b32bec64e776e3ca
WY0809 <105807155+WY0809@users.noreply.github.com>
2024-04-29 10:28:02 +0800

    Time: 17 ms (93.61%), Space: 17.8 MB (93.08%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -30,28 +30,55 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     void sub(vector<int>& nums, int index, vector<int>& sequence, vector<vector<int>>& result) {
+//         if (sequence.size() > 1) {
+//             result.push_back(sequence);
+//         }
+        
+//         unordered_set<int> used; // 用於去除重複的數字
+//         for (int i = index; i < nums.size(); ++i) {
+//             if ((sequence.empty() || nums[i] >= sequence.back()) && used.find(nums[i]) == used.end()) {
+//                 sequence.push_back(nums[i]);
+//                 sub(nums, i + 1, sequence, result);
+//                 sequence.pop_back();
+//                 used.insert(nums[i]);
+//             }
+//         }
+//     }
+    
+//     vector<vector<int>> findSubsequences(vector<int>& nums) {
+//         vector<vector<int>> result;
+//         vector<int> sequence;
+//         sub(nums, 0, sequence, result);
+//         return result;
+//     }
+// };
 class Solution {
 public:
-    void sub(vector<int>& nums, int index, vector<int>& sequence, vector<vector<int>>& result) {
-        if (sequence.size() >= 2) {
-            result.push_back(sequence);
-        }
-        
-        unordered_set<int> used; // 用於去除重複的數字
-        for (int i = index; i < nums.size(); ++i) {
-            if ((sequence.empty() || nums[i] >= sequence.back()) && used.find(nums[i]) == used.end()) {
-                sequence.push_back(nums[i]);
-                sub(nums, i + 1, sequence, result);
-                sequence.pop_back();
-                used.insert(nums[i]);
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        vector<int>onesub;
+        vector<vector<int>>res;
+        helper(0,nums,onesub,res);
+        return res;
+    }
+    void helper(int pos,const vector<int>&nums, vector<int>&onesub,
+               vector<vector<int>>&res){
+           if(pos>=nums.size()){
+            if(onesub.size()>1){
+                res.push_back(onesub);
             }
-        }
+            return;
+           }
+           if(onesub.size()==0||nums[pos]>=onesub.back()){
+            onesub.push_back(nums[pos]);
+            helper(pos+1,nums,onesub,res);
+            onesub.pop_back();
+           }
+           if(onesub.size()==0||nums[pos]!=onesub.back()){
+            helper(pos+1,nums,onesub,res);
+           }
     }
     
-    vector<vector<int>> findSubsequences(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int> sequence;
-        sub(nums, 0, sequence, result);
-        return result;
-    }
 };
