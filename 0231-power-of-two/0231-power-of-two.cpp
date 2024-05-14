d077f48e872908862fd944131ce6c99863d8d45f
WY0809 <105807155+WY0809@users.noreply.github.com>
2024-05-14 14:30:05 +0800

    Time: 0 ms (100%), Space: 7.1 MB (96.47%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-	    return n < 1 ? false : (n & (n - 1)) == 0 ? true : false;
+	    if(n < 1) return false;
+
+        return !(n & (n-1));
     }
 };
