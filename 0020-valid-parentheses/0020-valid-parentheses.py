a705a3a808edae525dfa3c9393b0506d359b2ac9
WY0809 <105807155+WY0809@users.noreply.github.com>
2025-03-26 15:12:26 +0800

    Time: 0 ms (100%), Space: 18.1 MB (10.26%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,21 +3,14 @@ class Solution:
         if len(s) % 2 != 0:
             return False
         hashmap = {')':'(', ']':'[', '}':'{'}
-
-        left = ['(', '[', '{']
-        # right = [')', ']', '}']
         stack = []
-        for i, n in enumerate(s):
-            # print(i,n)
-            if n in left:
-                stack.append(n)
-                # print(n)
-            else:
-                # print(hashmap[n])
-                # print(stack[-1])
-                if len(stack) == 0 or hashmap[n] != stack[-1]:
+        
+        for i in s:   
+            if i in hashmap:
+                if not stack or hashmap[i] != stack[-1]:
                     return False
                 stack.pop()
-        if len(stack) != 0:
-            return False
-        return True         
+            else:
+                stack.append(i)
+
+        return True if not stack else False
