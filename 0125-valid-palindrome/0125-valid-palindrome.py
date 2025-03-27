3b7c6faab5b0e6490eb5b952f64a5b10115f68db
WY0809 <105807155+WY0809@users.noreply.github.com>
2025-03-27 16:38:47 +0800

    Time: 11 ms (36.24%), Space: 18.1 MB (70.15%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,9 +3,9 @@ class Solution:
         l, r = 0, len(s) - 1
 
         while l < r:
-            while l < r and not self.alphaNum(s[l]):
+            while l < r and not s[l].isalnum():
                 l += 1
-            while r > l and not self.alphaNum(s[r]):
+            while r > l and not s[r].isalnum():
                 r -= 1
             if s[l].lower() != s[r].lower():
                 return False
