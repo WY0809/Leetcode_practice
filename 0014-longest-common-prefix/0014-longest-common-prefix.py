bbb50c43071a1c815443eb0a28c763e0c1b993b9
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-01-14 12:49:07 +0100

    Time: 3 ms (33.39%), Space: 12.6 MB (31.44%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -11,23 +11,3 @@ class Solution(object):
                     return ans 
             ans += ch
         return ans
-
-
-        # for i in range(k):
-        #     for j in strs[1:]:
-        #         if i+1 > len(j) or j[i] != strs[0][i]:
-        #             return ans 
-        #     ans += strs[0][i]
-        # return ans
-
-
-# class Solution(object):
-#     def longestCommonPrefix(self, strs):
-#         # if not strs:
-#         #     return ""
-
-#         first = strs[0]
-#         for i, ch in enumerate(first):
-#             if not all(i < len(s) and s[i] == ch for s in strs[1:]):
-#                 return first[:i]
-#         return first
