4802ca1574a1e17ea412976d5b4f60ce158a73de
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-01-14 12:22:45 +0100

    Time: 3 ms (33.39%), Space: 12.5 MB (31.44%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -8,16 +8,11 @@ class Solution(object):
         n = len(strs)
         k = len(strs[0])
         for i in range(k):
-            # print(strs[0][i])
-            ok = True
             stop = False
             for j in strs[1:]:
-                
-                print(j)
                 if i+1 > len(j):
                     stop = True
                     break 
-                print(j[i], strs[0][i])
                 if j[i] != strs[0][i]:
                     stop = True
                     ok = False
@@ -25,9 +20,6 @@ class Solution(object):
             if stop:
                 break
 
-            if ok:
-                ans += strs[0][i]
-                print(ans)
+            ans += strs[0][i]
 
-        # print(ans)
         return ans
