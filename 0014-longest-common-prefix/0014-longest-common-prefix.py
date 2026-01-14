d6c9114af9c0924e0817671ca58b96486b8f07b4
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-01-14 12:41:23 +0100

    Time: 2 ms (38.28%), Space: 12.6 MB (31.44%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,36 +1,38 @@
-# class Solution(object):
-#     def longestCommonPrefix(self, strs):
-#         """
-#         :type strs: List[str]
-#         :rtype: str
-#         """
-#         ans = ""
-#         n = len(strs)
-#         k = len(strs[0])
-#         for i in range(k):
-#             stop = False
-#             for j in strs[1:]:
-#                 if i+1 > len(j):
-#                     stop = True
-#                     break 
-#                 if j[i] != strs[0][i]:
-#                     stop = True
-#                     break
-#             if stop:
-#                 break
-
-#             ans += strs[0][i]
-
-#         return ans
-
-
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        # if not strs:
-        #     return ""
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        ans = ""
+        n = len(strs)
+        k = len(strs[0])
+        
+        # first = strs[0]
+        # for i, ch in enumerate(first):
 
-        first = strs[0]
-        for i, ch in enumerate(first):
-            if not all(i < len(s) and s[i] == ch for s in strs[1:]):
-                return first[:i]
-        return first
+        for i in range(k):
+            stop = False
+            for j in strs[1:]:
+                if i+1 > len(j) or j[i] != strs[0][i]:
+                    stop = True
+                    break 
+        
+            if stop:
+                return ans
+
+            ans += strs[0][i]
+
+        return ans
+
+
+# class Solution(object):
+#     def longestCommonPrefix(self, strs):
+#         # if not strs:
+#         #     return ""
+
+#         first = strs[0]
+#         for i, ch in enumerate(first):
+#             if not all(i < len(s) and s[i] == ch for s in strs[1:]):
+#                 return first[:i]
+#         return first
