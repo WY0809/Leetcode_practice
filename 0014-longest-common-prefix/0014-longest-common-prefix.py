05d46318af973b7d4920f456f941332323f19fdc
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-01-14 12:22:53 +0100

    Time: 4 ms (18.71%), Space: 12.4 MB (93.47%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -10,13 +10,12 @@ class Solution(object):
         for i in range(k):
             stop = False
             for j in strs[1:]:
-                if i+1 > len(j):
+                if i+1 > len(j) or j[i] != strs[0][i]:
                     stop = True
                     break 
-                if j[i] != strs[0][i]:
-                    stop = True
-                    ok = False
-                    break
+                # if :
+                #     stop = True
+                #     break
             if stop:
                 break
 
