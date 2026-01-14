1722832baa875616ab53b9b79a6d441eb4dfeb28
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-01-14 12:37:42 +0100

    Time: 4 ms (18.71%), Space: 12.5 MB (31.44%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,24 +1,36 @@
+# class Solution(object):
+#     def longestCommonPrefix(self, strs):
+#         """
+#         :type strs: List[str]
+#         :rtype: str
+#         """
+#         ans = ""
+#         n = len(strs)
+#         k = len(strs[0])
+#         for i in range(k):
+#             stop = False
+#             for j in strs[1:]:
+#                 if i+1 > len(j):
+#                     stop = True
+#                     break 
+#                 if j[i] != strs[0][i]:
+#                     stop = True
+#                     break
+#             if stop:
+#                 break
+
+#             ans += strs[0][i]
+
+#         return ans
+
+
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        ans = ""
-        n = len(strs)
-        k = len(strs[0])
-        for i in range(k):
-            stop = False
-            for j in strs[1:]:
-                if i+1 > len(j):
-                    stop = True
-                    break 
-                if j[i] != strs[0][i]:
-                    stop = True
-                    break
-            if stop:
-                break
+        if not strs:
+            return ""
 
-            ans += strs[0][i]
-
-        return ans
+        first = strs[0]
+        for i, ch in enumerate(first):
+            if not all(i < len(s) and s[i] == ch for s in strs[1:]):
+                return first[:i]
+        return first
