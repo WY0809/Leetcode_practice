6fda398b6f06dad96cb002f084a4c227697ca0ad
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-01-15 18:33:29 +0100

    Time: 27 ms (41.68%), Space: 16.8 MB (45.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,18 @@
+# from collections import defaultdict
+
 class Solution(object):
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-               
-        groups = {}
+        # groups = {}
+        groups = defaultdict(list)
+
         for i in strs:
             key = tuple(sorted(i))
-            if key not in groups:
-                groups[key] = []
+            # if key not in groups:
+            #     groups[key] = []
             groups[key].append(i)
 
         return groups.values()
