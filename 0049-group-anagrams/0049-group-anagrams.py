4f3de6afc144fde3f291f20e9acda4d0e5fe2477
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-01-15 18:24:53 +0100

    Time: 16 ms (90.1%), Space: 16.2 MB (59.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -8,15 +8,8 @@ class Solution(object):
         groups = {}
         for i in strs:
             key = "".join(sorted(i))
-            print(key)
             if key not in groups:
                 groups[key] = []
-                
             groups[key].append(i)
-
-        # for i in groups:
-        #     for j in strs:
-        #         if i == "".join(sorted(j)):
-        #             groups[i].append(j)
 
         return list(groups.values())
