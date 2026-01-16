68d1ab64321eb3a2ffea94883d9c2ad8e206acfc
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-01-16 16:33:20 +0100

    Time: 7 ms (85.52%), Space: 14.5 MB (47.85%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,11 +6,10 @@ class Solution(object):
         :rtype: List[int]
         """
         dic = defaultdict(int)
-
         for i in nums:
             dic[i] += 1
 
         dic = sorted(dic.items(), key=lambda x: -x[1])
 
-        ans =[i for i,j in dic[:k]]
+        ans =[num for num, _ in dic[:k]]
         return ans
