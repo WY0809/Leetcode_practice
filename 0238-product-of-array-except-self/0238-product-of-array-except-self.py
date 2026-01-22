1e64001af20c0f52bfeac45c332dd6bff283f31a
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-01-22 15:07:38 +0100

    Time: 19 ms (98.77%), Space: 19.4 MB (95.73%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        mul = 1
+        zero = 0
+        
+        for i in nums:
+            if i != 0:
+                mul *= i
+            else:
+                zero += 1
+
+        print(mul)
+        print(zero)
+
+        ans = []
+        if zero > 1:
+            ans = [0] * len(nums)
+        elif zero == 1:
+            for i in nums:
+                if i != 0:
+                    ans.append(0)
+                else:
+                    ans.append(mul)
+        else:
+            for i in nums:
+                ans.append(mul/i)
+                
+            
+        return ans
+
