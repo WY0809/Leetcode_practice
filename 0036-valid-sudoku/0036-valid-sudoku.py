9bfc92abb111097761af7260024ed7f3e49b0c2e
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-01-23 15:26:25 +0100

    Time: 0 ms (100%), Space: 12.5 MB (23.2%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -5,21 +5,22 @@ class Solution(object):
         :rtype: bool
         """
         seen = set()
-        # dic[board[0][0]] = 
+        
         for i in range(9):
             for j in range(9):
                 num = board[i][j]
-                if num != ".":
-                    box = i // 3 * 3 + j // 3
-                    
-                    temp = (
-                    ("row",i,num),
-                    ("col",j,num),
-                    ("box",box,num)
-                    )
-                    for k in temp:
-                        if k in seen:
-                            return False
-                        seen.add(k)
+                if num == ".":
+                    continue
+
+                box = (i // 3) * 3 + (j // 3)
+    
+                for k in (
+                ("row",i,num),
+                ("col",j,num),
+                ("box",box,num)
+                ):
+                    if k in seen:
+                        return False
+                    seen.add(k)
         return True
                     
