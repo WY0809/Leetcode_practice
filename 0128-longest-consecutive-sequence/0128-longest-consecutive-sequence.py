72ec7ae8ca3ca56f938da52ddc2c81d15ba88650
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-03-03 07:32:17 +0100

    Time: 38 ms (95.32%), Space: 36.6 MB (59.27%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -6,10 +6,8 @@ class Solution:
         for num in nums_set:
             if num - 1 not in nums_set:
                 current_num = num
-                # count = 1
 
                 while current_num in nums_set:
-                    # count += 1
                     current_num += 1
 
                 ans = max(ans, current_num - num)
