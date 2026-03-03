dfaf59af2d9437844c09e22eae43872721a0ebfb
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-03-03 06:55:53 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -3,15 +3,12 @@ class Solution:
         if nums == []:
             return 0
 
-
         s = sorted(set(nums))
-        print(s)
         n = len(s)
         i = s[0]
         count = 1
         ans = 1
         for j in range(1,n):
-            print(s[j], i+1)
             if s[j] == i+1:
                 count += 1
                 if count > ans:
