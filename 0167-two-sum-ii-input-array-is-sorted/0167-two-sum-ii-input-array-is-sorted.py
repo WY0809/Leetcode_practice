1cce338261d8326b208e3131dda2022cf7d886fb
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-03-05 11:30:06 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -4,9 +4,9 @@ class Solution:
                 
         while left < right :
             current = numbers[left] + numbers[right]
-            if current > target:
-                right -= 1
+            if current == target:
+                return [left+1, right+1]
             elif current < target:
                 left += 1
             else:
-                return [left+1, right+1]    
+                right -= 1
