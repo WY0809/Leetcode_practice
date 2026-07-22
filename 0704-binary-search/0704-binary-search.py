bf8ae7ce566c6345362874a7854e265288ae371a
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-07-22 17:19:32 +0800

    Time: 0 ms (100%), Space: 20.6 MB (34.68%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        L, R = 0, len(nums) - 1
+
+        while L <= R:
+            mid = (L + R) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if target > nums[mid]:
+                L = mid + 1
+            else:
+                R = mid - 1
+        return -1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
