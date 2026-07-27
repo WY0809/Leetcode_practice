a57864551b16a7197ba57b5d67a69b6dae293f7d
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-07-27 12:33:09 +0800

    Time: 3 ms (89.7%), Space: 22.6 MB (94.28%) - LeetHub - Time: O(n log n), Space: O(n)

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,15 +1,12 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        dic = defaultdict(int)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = defaultdict(int)
+
         for i in nums:
-            dic[i] += 1
+            freq[i] += 1
+        
+        return sorted(freq, key = freq.get, reverse=True)[:k]
 
-        dic = sorted(dic.items(), key=lambda x: -x[1])
-
-        ans =[num for num, _ in dic[:k]]
-        return ans
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
