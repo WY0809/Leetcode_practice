48c8c626e38f850a6fa2ee5c0c5bac74d8a4d4aa
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-07-29 18:50:55 +0800

    Time: 467 ms (95.26%), Space: 22.4 MB (31.37%) - LeetHub - Time: O(n^2), Space: O(n)

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,24 +3,31 @@ class Solution:
         nums.sort()
         n = len(nums)
         ans = []
+        
         for i in range(n-2):
             if nums[i] > 0:
                 break
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            left, right = i+1, n-1
-            while left < right:
-                current = nums[i] + nums[left] + nums[right]
-                if current > 0:
-                    right -= 1
-                elif current < 0:
-                    left += 1
+                
+            L, R = i+1, n-1
+            while L < R:
+                total = nums[i] + nums[L] + nums[R]
+                if total > 0:
+                    R -= 1
+                elif total < 0:
+                    L += 1
                 else:
-                    ans.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-                    while left < right and nums[left] == nums[left-1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right+1]:
-                        right -= 1
+                    ans.append([nums[i], nums[L], nums[R]])
+                    L += 1
+                    R -= 1
+                    
+                    while L < R and nums[L] == nums[L-1]:
+                        L += 1
+                    while L < R and nums[R] == nums[R+1]:
+                        R -= 1
         return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
