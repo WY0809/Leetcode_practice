b44711a4c047142bc07b4b6f1afe538275c6b312
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-08-24 11:49:01 +0800

    Time: 0 ms (100%), Space: 19.2 MB (93.09%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def rob_linear(nums):
+            n = len(nums)
+            dp = [0] * (n + 1)
+            dp[1] = nums[0]
+
+            for i in range(2, n + 1):
+                dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
+
+            return dp[n]
+            
+        if len(nums) == 1:
+            return nums[0]
+
+        return max(rob_linear(nums[1:]), rob_linear(nums[:-1]))
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
