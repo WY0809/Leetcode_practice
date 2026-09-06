35230f137ca9c4b4e2402e7f882d6d4569f338fa
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-09-06 17:08:28 +0800

    Time: 0 ms (100%), Space: 8.5 MB (31.96%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0202-happy-number/0202-happy-number.c
@@ -0,0 +1,22 @@
+bool isHappy(int n) {
+    while(n != 1 && n != 4){
+        int Sum = 0;
+
+        while(n){
+            int temp = n % 10;
+            Sum += temp * temp;
+            n /= 10;
+        }
+        
+        n = Sum;
+    }
+
+    if (n == 1)
+        return true;
+    else
+        return false;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
