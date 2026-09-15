8b9af15a6c6a9d85a1942014f97c10b5ca0df3e3
WY0809 <105807155+WY0809@users.noreply.github.com>
2026-09-15 14:55:20 +0800

    Time: 0 ms (100%), Space: 27.8 MB (80.48%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev = None
+        curr = slow.next
+        slow.next = None
+
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        first = head
+        second = prev
+
+        while second:
+            next_first = first.next
+            next_second = second.next
+
+            first.next = second
+            second.next = next_first
+
+            first = next_first
+            second = next_second
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
